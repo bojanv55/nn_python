6fb211cbdffe40bdc4076edeaa9f49a5b87077f9
default <bojanv55@gmail.com>
2019-09-16 20:57:28 +0200

    cnn

--- cnn.py
+++ cnn.py
@@ -1,15 +1,35 @@
 import matplotlib.pyplot as plt
 import imageio
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
-im = imageio.imread("./Figures/chapel.png")
-print(im.shape)
-plt.imshow(im)
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./datasets/MNIST_data/", one_hot=True)
 
-im_red = im[:,:,0]
-print(im_red.shape)
-plt.imshow(im_red, cmap='gray')
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (5,5), input_shape=(28, 28, 1), activation='relu'),
+    keras.layers.MaxPool2D((2,2)),
+    keras.layers.Conv2D(64, (5, 5), input_shape=(28, 28, 1), activation='relu'),
+    keras.layers.MaxPool2D((2,2)),
+    keras.layers.Dense(256, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
 
-im_blue = im[:,:,2]
-plt.imshow(im_blue, cmap='gray')
+model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-plt.show()
+input_layer = tf.reshape(mnist.train.images, [-1, 28, 28, 1])
+lbls = [np.argmax(l) for l in mnist.train.labels]
+lbls = np.reshape(lbls, 55000)
+
+model.fit(input_layer, lbls, epochs=5, steps_per_epoch=1000)
+
+input_t_layer = tf.reshape(mnist.test.images, [-1, 28, 28, 1])
+lblst = [np.argmax(l) for l in mnist.test.labels]
+lblst = np.reshape(lbls, 55000)
+
+test_loss, test_acc = model.evaluate(input_t_layer, lblst)
+
+print('Test accuracy:', test_acc)
